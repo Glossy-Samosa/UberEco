5ddc92b8046d30d13dd9fc23c606abbaecdd34d0
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-07 12:57:09 -0700

    (feat) added maps folder with request file

--- uberEco/src/main.js
+++ uberEco/src/main.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, Navigator } from 'react-native';
+import {
+  StyleSheet,
+  Navigator
+} from 'react-native';
 
 var Login = require('./components/authentication/login');
 var Signup = require('./components/authentication/signup');
+var Maps = require('./components/maps/request');
 
 var ROUTES = {
-  login: Login,
-  signup: Signup
+	login: Login,
+	signup: Signup,
+  maps: Maps
 };
 
 module.exports = React.createClass({
@@ -15,19 +20,19 @@ module.exports = React.createClass({
     return <Component route={route} navigator={navigator} />;
 	},
   render: function() {
-  	return (
-      <Navigator
+    return (
+      <Navigator 
         style={styles.container}
         initialRoute={{name: 'login'}}
         renderScene={this.renderScene}
         configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }}
       />
-  	);
+    );
   }
 });
 
 var styles = StyleSheet.create({
-	container: {
-		flex: 1
-	}
-})
+  container: {
+  	flex: 1
+  }
+});
